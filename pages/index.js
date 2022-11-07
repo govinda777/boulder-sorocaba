9d977fbef4a6a69d2e841f92b9dc97ebcee117c7
Govinda <govinda777@gmail.com>
2022-11-06 22:10:12 -0300

    up

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import { useWeb3 } from '@3rdweb/hooks'
 import { useEffect } from 'react'
 import { client } from '../lib/sanityClient'
 
-
 const style = {
   wrapper: ``,
   walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
@@ -13,41 +12,24 @@ const style = {
   details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
 }
 
+
+
 export default function Home() {
-  const { address, connectWallet } = useWeb3()
-/*
-  const welcomeUser = (userName, toastHandler = toast) => {
-    toastHandler.success(
-      `Welcome back${userName !== 'Unnamed' ? ` ${userName}` : ''}!`,
-      {
-        style: {
-          background: '#04111d',
-          color: '#fff',
-        },
-      }
-    )
-  }
-*/
-  useEffect(() => {
-    if (!address) return
-    ;(async () => {
-      const userDoc = {
-        _type: 'users',
-        _id: address,
-        userName: 'Unnamed',
-        walletAddress: address,
-      }
 
-      const result = await client.createIfNotExists(userDoc)
-
-      welcomeUser(result.userName)
-    })()
-  }, [address])
+  const { account, address, chainId, connectWallet, disconnectWallet, getNetworkMetadata } = useWeb3();
 
   return (
+    
     <div className={style.wrapper}>
+      {
+        console.log(address)
+      }
+      {
+        console.log(account)
+      }
+      {
       
-      {address ? (
+      !address ? (
         <>
           <Header />
           <Hero />
