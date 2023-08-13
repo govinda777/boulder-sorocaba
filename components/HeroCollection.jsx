6a168f6182974d856174d158cc86cbf87aeddfee
govinda777 <govinda777@gmail.com>
2023-08-13 12:09:05 -0300

    Melhoria no layout da página da loja

--- components/HeroCollection.jsx
+++ components/HeroCollection.jsx
@@ -1,30 +1,47 @@
 
-import { Card, Image } from 'antd';
+import { Card, Image, Avatar, Typography } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 
-const HeroCollection = ({bannerImageUrl}) => {
+
+const HeroCollection = ({ collection }) => {
+
+    const { Title, Text } = Typography;
+    const { Meta } = Card;
 
     return (
         <>
-        <Card
-            cover={ <Image
-            width="100%"
-            height="400px"
-            src={
-                bannerImageUrl
-            }
-            />
-        }
-            bordered={false}
-            style={{ textAlign: 'center' }}
-        >
-            <Card.Meta
-            title="Título do Banner 1"
-            description="Descrição do Banner 1"
-            />
-        </Card>
+            <Card
+                style={{
+                    width: '100%',
+                    height: '500px'
+                }}
+                cover={
+                    <Image
+                    width="100%"
+                    height="300px"
+                    src={
+                        collection.bannerImageUrl
+                    }
+                />
+                }
+            >
+                <Meta
+                    avatar={
+                    <Avatar
+                        size={150}
+                        src={
+                          collection?.imageUrl
+                            ? collection.imageUrl
+                            : 'https://via.placeholder.com/200'
+                        }
+                      />}
+                    title={<Title level={1}>{collection?.title}</Title>}
+                    description={collection?.description}
+                />
+            </Card>
         </>
     )
-      
+
 }
 
 export default HeroCollection
--- pages/collections/[collectionId].jsx
+++ pages/collections/[collectionId].jsx
@@ -7,7 +7,7 @@ import { client } from '../../lib/sanityClient'
 import { ThirdwebSDK } from '@3rdweb/sdk'
 import Header from '../../components/Header'
 import NFTCard from '../../components/NFTCard'
-import { Card, Divider, Typography, Row, Col, Image, Statistic, Avatar, Meta } from 'antd';
+import { Card, Divider, Typography, Row, Col, Image, Statistic, Avatar, Meta, Layout, Space } from 'antd';
 import { InstagramOutlined, TwitterOutlined, MoreOutlined, GlobalOutlined, EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import HeroCollection from '../../components/HeroCollection'
 
@@ -30,11 +30,11 @@ const Collection = () => {
 
   useEffect(() => {
     if (!nftModule) return
-    ;(async () => {
-      const nfts = await nftModule.getAll()
+      ; (async () => {
+        const nfts = await nftModule.getAll()
 
-      setNfts(nfts)
-    })()
+        setNfts(nfts)
+      })()
   }, [nftModule])
 
   const marketPlaceModule = useMemo(() => {
@@ -52,12 +52,12 @@ const Collection = () => {
   useEffect(() => {
 
     if (!marketPlaceModule) return
-    ;(async () => {
+      ; (async () => {
         const result = await marketPlaceModule.getAllListings()
         console.log("console.log({result})")
-        console.log({result})
-      setListings(result)
-    })()
+        console.log({ result })
+        setListings(result)
+      })()
   }, [marketPlaceModule])
 
   const fetchCollectionData = async (sanityClient = client) => {
@@ -74,12 +74,12 @@ const Collection = () => {
     }`
 
     const collectionData = await sanityClient.fetch(query)
-    
+
     const collectionZero = collectionData[0];
 
-    console.log({collectionZero})
-    if(collectionData && collectionData.length > 0) {        
-        await setCollection(collectionZero)
+    console.log({ collectionZero })
+    if (collectionData && collectionData.length > 0) {
+      await setCollection(collectionZero)
     }
   }
 
@@ -90,113 +90,80 @@ const Collection = () => {
   const { Title, Text } = Typography;
 
   const bannerImageUrl = collection?.bannerImageUrl
-  ? collection.bannerImageUrl
-  : 'https://via.placeholder.com/200';
+    ? collection.bannerImageUrl
+    : 'https://via.placeholder.com/200';
+
+  const { Footer, Sider, Content } = Layout;
+
+  const headerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    height: 64,
+    paddingInline: 50,
+    lineHeight: '64px',
+    backgroundColor: '#7dbcea',
+  };
+
+  const contentStyle = {
+    textAlign: 'center',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#108ee9',
+  };
+
+  const siderStyle = {
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#3ba0e9',
+  };
+
+  const footerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#7dbcea',
+  };
 
   return (
     <>
-      <HeroCollection bannerImageUrl={bannerImageUrl}/>
-      <Avatar
-          size={160}
-          src={
-            collection?.imageUrl
-              ? collection.imageUrl
-              : 'https://via.placeholder.com/200'
-          }
-        />
-        <Title level={1}>{collection?.title}</Title>
-        <Row justify="center">
-        <Card style={{ width: 300, marginTop: 16 }} bordered={false}>
-          <Row justify="space-between">
-            <Col>
-              <Statistic title="Items" value={nfts.length} />
-            </Col>
-            <Col>
-              <Statistic title="Owners" value={collection?.allOwners ? collection.allOwners.length : ''} />
-            </Col>
-          </Row>
-        </Card>
-      </Row>
-      <Row justify="center" gutter={16}>
-        {nfts.map((nftItem, id) => (
-          <Col span={8}>
-            <NFTCard
-              key={id}
-              nftItem={nftItem}
-              title={collection?.title}
-              listings={listings}
-            />
-          </Col>
-        ))}
-      </Row>
+      <Header />
+      <Layout>
+        <Sider style={siderStyle}>Sider</Sider>
+        <Layout>
+          <Content style={contentStyle}>
+            <HeroCollection collection={collection} />
+            <Row justify="center">
+              <Card style={{ width: 300, marginTop: 16 }} >
+                <Row justify="space-between">
+                  <Col>
+                    <Statistic title="Items" value={nfts.length} />
+                  </Col>
+                  <Col>
+                    <Statistic title="Owners" value={collection?.allOwners ? collection.allOwners.length : ''} />
+                  </Col>
+                </Row>
+              </Card>
+            </Row>
+            <Row justify="center" gutter={16}>
+              {nfts.map((nftItem, id) => (
+                <Col span={8}>
+                  <NFTCard
+                    key={id}
+                    nftItem={nftItem}
+                    title={collection?.title}
+                    listings={listings}
+                  />
+                </Col>
+              ))}
+            </Row>
+
+          </Content>
+          <Footer style={footerStyle}>Footer</Footer>
+        </Layout>
+      </Layout>
     </>
   );
-  
 }
-
-/*
-      <Header />
-
-      <Row justify="center">
-        <Avatar
-          size={160}
-          src={
-            collection?.imageUrl
-              ? collection.imageUrl
-              : 'https://via.placeholder.com/200'
-          }
-        />
-      </Row>
-      <Row justify="center">
-        <Card style={{ width: 300, marginTop: 16 }} bordered={false}>
-          <GlobalOutlined />
-          <Divider type="vertical" />
-          <InstagramOutlined />
-          <Divider type="vertical" />
-          <TwitterOutlined />
-          <Divider type="vertical" />
-          <MoreOutlined />
-        </Card>
-      </Row>
-      <Row justify="center">
-        <Title level={1}>{collection?.title}</Title>
-      </Row>
-      <Row justify="center">
-        <Text>Created by <Text type="secondary">{collection?.creator}</Text></Text>
-      </Row>
-      <Row justify="center">
-        <Card style={{ width: 300, marginTop: 16 }} bordered={false}>
-          <Row justify="space-between">
-            <Col>
-              <Statistic title="Items" value={nfts.length} />
-            </Col>
-            <Col>
-              <Statistic title="Owners" value={collection?.allOwners ? collection.allOwners.length : ''} />
-            </Col>
-            <Col>
-              <Statistic title="Floor Price" value={collection?.floorPrice} prefix={<img src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg" alt="eth" />} />
-            </Col>
-            <Col>
-              <Statistic title="Volume Traded" value={collection?.volumeTraded} prefix={<img src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg" alt="eth" />} />
-            </Col>
-          </Row>
-        </Card>
-      </Row>
-      <Row justify="center">
-        <Text>{collection?.description}</Text>
-      </Row>
-      <Row justify="center" gutter={16}>
-        {nfts.map((nftItem, id) => (
-          <Col span={8}>
-            <NFTCard
-              key={id}
-              nftItem={nftItem}
-              title={collection?.title}
-              listings={listings}
-            />
-          </Col>
-        ))}
-      </Row>
-*/
 
 export default Collection
